cd5f88615f24f7e69027cf24f57640444ad36c40
Jung, DaeCheon <daejung@ea.com>
2019-05-02 17:52:25 +0900

    main scene

--- Classes/Scenes.cpp
+++ Classes/Scenes.cpp
@@ -22,23 +22,20 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-#ifndef __HELLOWORLD_SCENE_H__
-#define __HELLOWORLD_SCENE_H__
+#include "Scenes.h"
+#include "SimpleAudioEngine.h"
 
-#include "cocos2d.h"
-
-class HelloWorld : public cocos2d::Scene
+bool SceneMain::init()
 {
-public:
-    static cocos2d::Scene* createScene();
+    gui::inst()->initDefaultWithSpriteCache();
+    this->loadFromJson("main", "main.json");
+    return true;
+}
 
-    virtual bool init();
+void SceneMain::callback(Ref* pSender, int from, int link) {
     
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
-    
-    // implement the "static create()" method manually
-    CREATE_FUNC(HelloWorld);
-};
+}
 
-#endif // __HELLOWORLD_SCENE_H__
+const string SceneMain::getText(const string& defaultString, int id) {
+    return defaultString;
+}
